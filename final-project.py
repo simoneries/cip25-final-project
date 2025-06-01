7fc536a9491d8520af482a47485c18d35be4ec38
simoneries <192712243+simoneries@users.noreply.github.com>
2025-06-01 18:00:24 +0200

    Set the final project file

---
+++ final-project.py
@@ -0,0 +1,17 @@
+"""
+Je veux créer un outil pour me mettre à jour sur les concerts qui sortent avec l'api de ticketmaster.
+First milestone : Recevoir des données de l'API
+Second milestone : A partir des données, demander à l'utilisateur quels concerts il aimerait voir et en sortir les infos. 
+"""
+import requests
+
+
+#First milestone : Get API data. 
+
+def main():
+
+    pass
+
+def get_api_data():
+
+    pass
